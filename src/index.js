2445ccbd83c49ee45c62231ac517891ef7519ac6
heyset <c.inaciomatheus@gmail.com>
2021-07-09 18:40:25 -0300

    teste fix deploy

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import rootReducer from './reducers';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="hubbusters">
       <Provider store={ createStore(rootReducer) }>
         <App />
       </Provider>
